4bfdb88615063593717d5545df03efe9a727217e
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-12-24 18:49:50 +0300

    Visual Studio Installer Setup Project

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,6 +10,45 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            int number1, number2;
+            string operation;
+            int result = 0;
+            Console.Write("First Number:");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Second Number: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Addition + Subtraction - Multiplication * Division / : ");
+            operation = Console.ReadLine();
+
+            if (operation == "+")
+            {
+                result = number1 + number2;
+                Console.WriteLine("Result: " + result);
+                Console.ReadKey();
+            }
+            else if (operation == "-")
+            {
+                result = number1 - number2;
+                Console.WriteLine("Result: " + result);
+                Console.ReadKey();
+            }
+            else if (operation == "*")
+            {
+                result = number1 * number2;
+                Console.WriteLine("Result: " + result);
+                Console.ReadKey();
+            }
+            else if (operation == "/")
+            {
+                result = number1 / number2;
+                Console.WriteLine("Result: " + result);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("ERROR!");
+                Console.ReadKey();
+            }
         }
     }
 }
